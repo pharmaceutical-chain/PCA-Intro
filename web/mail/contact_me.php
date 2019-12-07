bdb822376ab45d737df671c3d1cac5cad27bb0d6
Lee Khai <leekhai18@gmail.com>
2019-12-07 17:52:33 +0700

    :rocket: last try for php mail

--- web/mail/contact_me.php
+++ web/mail/contact_me.php
@@ -19,7 +19,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "leekhai18@gmail.com"; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Introduction Platform Contact Form:  $name";
 $email_body = "You have received a new message from Introduction Platform Contact Form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@pharmachain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: muacodailove@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
